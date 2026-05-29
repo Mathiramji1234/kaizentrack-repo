987cdf11f9da44ad21f3616bd667deb46d34b99a
Mathi Ramji K <mathiramji2006@gmail.com>
2026-05-29 21:03:17 +0530

    Initial React setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center">
+      <h1 className="text-4xl font-bold">
+        KaizenTrack 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
